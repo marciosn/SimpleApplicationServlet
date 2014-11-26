16006bf65514a1ec7fafaddd7c531e8c6a3a264c
marciosn <marciosouzanobre@gmail.com>
2014-11-26 16:54:16 -0300

    adicionando spinner a pagina formulario

--- SampleServlet/WebContent/formulario.jsp
+++ SampleServlet/WebContent/formulario.jsp
@@ -10,19 +10,27 @@
 <link rel="stylesheet" type="text/css" href="./resources/bootstrap/css/mycss.css" />
 <link rel="stylesheet" type="text/css" href="./resources/bootstrap/css/component.css" />
 <link rel="stylesheet" type="text/css" href="./resources/bootstrap/css/bootstrap-responsive.css" />
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="./resources/bootstrap/css/jquery.datetimepicker.css" />
 
-<link rel="stylesheet" type="text/css" href="./resources/bootstrap/css/jquery-ui.css" />
+<!-- <link rel="stylesheet" type="text/css" href="./resources/bootstrap/css/jquery-ui.css" /> -->
 
 <script src="./resources/bootstrap/js/jquery-1.10.2.js"></script>
-<script src="./resources/bootstrap/spinner/js/jquery-ui.js"></script>
+<script src="./resources/bootstrap/js/jquery-ui.js"></script>
 
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
-  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+
+<!-- <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script> -->
 
  <script>
   $(function() {
     $( "#datepicker" ).datepicker();
+  });
+  </script>
+
+<script>
+  $(function() {
+    $( "#speed" ).selectmenu();
   });
   </script>
 
@@ -38,7 +46,19 @@
 			<caption>Formulário</caption>
 		</h2>
 		
+		<h2>Date Picker</h2>
 		<p>Date: <input type="text" id="datepicker"></p>
+		
+		
+		<select name="speed" id="speed">
+			<option selected="selected" >Selecione</option>
+      		<option>Slower</option>
+      		<option>Slow</option>
+      		<option>Medium</option>
+      		<option>Fast</option>
+      		<option>Faster</option>
+    	</select>
+		
 		
 		</div>
 	</div>
--- SampleServlet/WebContent/resources/bootstrap/js/jquery-ui.js
+++ SampleServlet/WebContent/resources/bootstrap/js/jquery-ui.js
@@ -8057,14 +8057,14 @@ function Datepicker() {
 		prevText: "Prev", // Display text for previous month link
 		nextText: "Next", // Display text for next month link
 		currentText: "Today", // Display text for current month link
-		monthNames: ["January","February","March","April","May","June",
-			"July","August","September","October","November","December"], // Names of months for drop-down and formatting
-		monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], // For formatting
-		dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"], // For formatting
-		dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"], // For formatting
-		dayNamesMin: ["Su","Mo","Tu","We","Th","Fr","Sa"], // Column headings for days starting at Sunday
+		monthNames: ["Janeiro","Fevereiro","Março","Abril","Maio","Junho",
+			"Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"], // Names of months for drop-down and formatting
+		monthNamesShort: ["Jan", "Fev", "Mai", "Abr", "Mar", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"], // For formatting
+		dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"], // For formatting
+		dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"], // For formatting
+		dayNamesMin: ["Dom","Seg","Ter","Qua","Qui","Sex","Sab"], // Column headings for days starting at Sunday
 		weekHeader: "Wk", // Column header for week of the year
-		dateFormat: "mm/dd/yy", // See format options on parseDate
+		dateFormat: "dd/mm/yy", // See format options on parseDate
 		firstDay: 0, // The first day of the week, Sun = 0, Mon = 1, ...
 		isRTL: false, // True if right-to-left language, false if left-to-right
 		showMonthAfterYear: false, // True if the year select precedes month, false for month then year
